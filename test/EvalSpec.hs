7ad06bb56a7e174e52d9ab12faeb57487e8a757c
arthur <arthur.teisseire@epitech.eu>
2020-01-02 13:29:02 +0100

    Add nested test for Eval

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -14,3 +14,5 @@ spec = describe "test evaluation" $ do
         eval (List [Symbol "+", Number 2, Number 3]) `shouldBe` Number 5
     it "test a simple substraction" $
         eval (List [Symbol "-", Number 2, Number 3]) `shouldBe` Number (-1)
+    it "test a nested addition" $
+        eval (List [Symbol "+", List [Symbol "+", Number 1, Number 2], List [Symbol "+", Number 3, Number 4]]) `shouldBe` Number 10
