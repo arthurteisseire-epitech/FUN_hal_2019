de6c04a012db177fd48f9b80d768d3c4c6bafd59
arthur <arthur.teisseire@epitech.eu>
2020-01-03 18:57:00 +0100

    Add mul, divide, modulo

--- src/Builtin.hs
+++ src/Builtin.hs
@@ -1,6 +1,7 @@
 module Builtin where
 
 import Expression
+import Data.Fixed
 
 
 add :: [Expr] -> Expr
@@ -9,9 +10,21 @@ add [Number num1, Number num2] = Number (num1 + num2)
 sub :: [Expr] -> Expr
 sub [Number num1, Number num2] = Number (num1 - num2)
 
+mul :: [Expr] -> Expr
+mul [Number num1, Number num2] = Number (num1 * num2)
+
+divide :: [Expr] -> Expr
+divide [Number num1, Number num2] = Number (num1 / num2)
+
+modulo :: [Expr] -> Expr
+modulo  [Number num1, Number num2] = Number (num1 `mod'` num2)
+
 env :: [(String, Expr)]
 env = [ ("+", Func add)
       , ("-", Func sub)
+      , ("*", Func mul)
+      , ("div", Func divide)
+      , ("mod", Func modulo)
       ]
 
 symbols :: [String]
