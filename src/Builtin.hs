6b12f50c14fd5376c7c19bd0c48d39693f5750f5
arthur <arthur.teisseire@epitech.eu>
2020-01-04 11:23:49 +0100

    Refactor builtins

--- src/Builtin.hs
+++ src/Builtin.hs
@@ -4,27 +4,15 @@ import Expression
 import Data.Fixed
 
 
-add :: [Expr] -> Expr
-add [Number num1, Number num2] = Number (num1 + num2)
-
-sub :: [Expr] -> Expr
-sub [Number num1, Number num2] = Number (num1 - num2)
-
-mul :: [Expr] -> Expr
-mul [Number num1, Number num2] = Number (num1 * num2)
-
-divide :: [Expr] -> Expr
-divide [Number num1, Number num2] = Number (num1 / num2)
-
-modulo :: [Expr] -> Expr
-modulo  [Number num1, Number num2] = Number (num1 `mod'` num2)
+arithmetic :: (Float -> Float -> Float) -> [Expr] -> Expr
+arithmetic f [Number num1, Number num2] = Number $ f num1 num2
 
 env :: [(String, Expr)]
-env = [ ("+", Func add)
-      , ("-", Func sub)
-      , ("*", Func mul)
-      , ("div", Func divide)
-      , ("mod", Func modulo)
+env = [ ("+", Func (arithmetic (+)))
+      , ("-", Func (arithmetic (-)))
+      , ("*", Func (arithmetic (*)))
+      , ("div", Func (arithmetic (/)))
+      , ("mod", Func (arithmetic mod'))
       ]
 
 symbols :: [String]
