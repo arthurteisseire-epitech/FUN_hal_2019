6ad32360d4f3541b40307a51d4732938f15bdba1
arthur <arthur.teisseire@epitech.eu>
2020-01-05 10:11:14 +0100

    Add interpret file mode when there is one argument

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,31 @@ module Main where
 
 import           Eval
 import           Parser
+import           System.Environment
 import           System.Exit
 import           System.IO
 
 main :: IO ()
-main = repl
+main = getArgs >>= interpret
+
+interpret :: [String] -> IO ()
+interpret args
+    | null args = repl
+    | otherwise = interpretFile args
+
+interpretFile :: [String] -> IO ()
+interpretFile args = openFile (head args) ReadMode >>= hGetContents >>= interpretLine . filter (/= '\n')
 
 repl :: IO ()
 repl = do
     isEof <- isEOF
     if isEof
-        then exitSuccess
+        then putStrLn "goodbye!"
         else do
             line <- getLine
-            print $ (eval . parse) line
+            interpretLine line
             hFlush stdout
             repl
+
+interpretLine :: String -> IO ()
+interpretLine = print . eval . parse
