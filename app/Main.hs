ac54a3e11d51e09fedba311e2232a6ecd67de043
arthur <arthur.teisseire@epitech.eu>
2020-01-07 22:55:53 +0100

    Refactor Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,28 @@
 module Main where
 
-import           Data.Char
-import           Eval
-import           Parser
-import           System.Environment
-import           System.IO
+import Control.Applicative
+import Data.Char
+import Eval
+import Parser
+import System.Environment
+import System.IO
+import Control.Monad (unless)
 
 main :: IO ()
 main = getArgs >>= interpret
 
 interpret :: [String] -> IO ()
-interpret args
-    | null args = repl
-    | otherwise = interpretFile args
+interpret [] = repl
+interpret args = interpretFile args
 
 interpretFile :: [String] -> IO ()
 interpretFile args = openFile (head args) ReadMode >>= hGetContents >>= putStrLn . interpretLine . filter isControl
 
 repl :: IO ()
-repl = do
-    isEof <- isEOF
-    if isEof
-        then putStrLn "goodbye!"
-        else do
-            interactLine interpretLine
-            repl
-
-interpretLine :: String -> String
-interpretLine = showEither . (fmap . fmap) eval parse
-
-showEither :: Show a => Either String a -> String
-showEither = either show show
+repl = ((unless :: Bool -> IO () -> IO ()) <$> isEOF) *> (interactLine interpretLine >> repl)
 
 interactLine :: (String -> String) -> IO ()
-interactLine f = do
-    s <- getLine
-    putStrLn (f s)
-    hFlush stdout
+interactLine f = getLine >>= \l -> putStrLn (f l) >> hFlush stdout
+
+interpretLine :: String -> String
+interpretLine s = either show show (eval <$> parse s)
